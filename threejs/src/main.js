14156bf193d94a73b1f95eef3f36f99fe6edd065
Kourpa <koopastroopa@gmail.com>
2019-06-22 22:03:23 -0500

    trying to get loading of obj to work

---
+++ threejs/src/main.js
@@ -0,0 +1,148 @@
+const fileChanged = function () {
+    const $mainFilePicker = $('#file-picker-input')
+    mainObjectFiles = $mainFilePicker[0].files;
+
+    const $mainTexturePicker = $('#texture-picker-input')
+    mainObjectTextures = $mainTexturePicker[0].files;
+
+    if (mainObjectFiles.length > 0 && mainObjectTextures.length > 0) {
+        init();
+        animate();
+    }
+}
+
+let mainObjectFiles;
+let mainObjectTextures;
+let container;
+let camera, scene, renderer;
+
+let mouseX = 0, mouseY = 0;
+
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+
+let object;
+
+function init() {
+
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.z = 250;
+
+    // scene
+
+    scene = new THREE.Scene();
+
+    var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+    scene.add(ambientLight);
+
+    var pointLight = new THREE.PointLight(0xffffff, 0.8);
+    camera.add(pointLight);
+    scene.add(camera);
+
+    // manager
+    function loadModel() {
+
+        object.traverse(function (child) {
+
+            if (child.isMesh) child.material.map = texture;
+
+        });
+
+        object.position.y = - 95;
+        scene.add(object);
+
+    }
+
+    var manager = new THREE.LoadingManager(loadModel);
+
+    manager.onProgress = function (item, loaded, total) {
+
+        console.log(item, loaded, total);
+
+    };
+
+    // texture
+
+    var textureLoader = new THREE.TextureLoader(manager);
+
+    var texture = textureLoader.load('../textures/UV_Grid_sm.jpg');
+
+    // model
+
+    function onProgress(xhr) {
+
+        if (xhr.lengthComputable) {
+
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log('model ' + Math.round(percentComplete, 2) + '% downloaded');
+
+        }
+
+    }
+
+    function onError() { }
+
+    var loader = new THREE.OBJLoader(manager);
+
+    loader.load('../models/male02.obj', function (obj) {
+
+        object = obj;
+
+    }, onProgress, onError);
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+    //
+
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function onDocumentMouseMove(event) {
+
+    mouseX = (event.clientX - windowHalfX) / 2;
+    mouseY = (event.clientY - windowHalfY) / 2;
+
+}
+
+//
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    render();
+
+}
+
+function render() {
+
+    camera.position.x += (mouseX - camera.position.x) * .05;
+    camera.position.y += (- mouseY - camera.position.y) * .05;
+
+    camera.lookAt(scene.position);
+
+    renderer.render(scene, camera);
+
+}
