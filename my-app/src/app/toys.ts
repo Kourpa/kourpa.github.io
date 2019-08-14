25382d1350495adc667e256aadbc60c50b203455
Kourpa <kourpa@computron.localdomain>
2019-08-14 15:27:56 -0500

    added url to toys

--- my-app/src/app/toys.ts
+++ my-app/src/app/toys.ts
@@ -1,15 +1,18 @@
 export class Toy {
   img: string;
+  url: string;
   description: string;
 }
 
 export const TOYS = [
   {
     img: 'assets/img/wordCloud.png',
+    url: 'http://kourpa.github.io/wordCloud/index.html',
     description: 'A simple application that uses D3 to represent a word cloud'
   },
   {
     img: 'assets/img/threejs.png',
+    url: 'http://kourpa.github.io/threejs2/index.html',
     description: 'Introduction to threejs'
   }
 ];
